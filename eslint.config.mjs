fcc8efdaf4032cfc0650b9eb230574474d723c2f
jonathanpv <jonpad512@gmail.com>
2025-11-01 20:03:32 -0500

    make build2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,8 @@ const eslintConfig = [
       '@next/next/no-page-custom-font': 'off',
       '@next/next/no-img-element': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'off'
+      '@typescript-eslint/no-unused-vars': 'off',
+      'react-hooks/exhaustive-deps': 'off'
     },
   },
 ];
